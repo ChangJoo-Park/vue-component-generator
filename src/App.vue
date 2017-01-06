24fb649d1e5ca31d2fd9dd42e412f90c8cdc93a0
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-06 20:06:11 +0900

    Add Props validation

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,129 @@
 <template lang="pug">
   #app
-    div.header
-    //- Left (Generator)
-    div.row
-      div.generator.col-md-6
-        h1 Vue Component Generator
-        div.options
-          div.card
-            label.label-header Template
-            select(v-model="newComponent.selectedTemplate")
-              option(v-for="option in options.template" v-bind:value="option") {{option}}
-          div.card
-            label.label-header Script
-            select(v-model="newComponent.selectedScript")
-              option(v-for="option in options.scripts" v-bind:value="option") {{option}}
-          div.card
-            label.label-header Style
-            select(v-model="newComponent.selectedStyle")
-              option(v-for="option in options.styles" v-bind:value="option") {{option}}
-          div.card
-            label.label-header Scoped Style
-              br
-              input(type="checkbox" v-model="newComponent.selectedScoped")
-              span.checkable on
-        div.component-form
-          div.card
+    nav.header
+      a.brand Vue Component Generator
+    div.flex.two
+      codemirror.container(v-bind:code="compiledComponent", v-bind:options="editorOption")
+      div.container
+        div
+          div
             label.label-header Component Name
             input(type="text", v-model="newComponent.name")
-          div.card
-            button(v-on:click="addNewProp") Add Prop
-            div(v-for="(prop, index) in newComponent.props")
-              label.label-header Props
-                input(type="text", placeholder="props" v-model="prop.name")
-              button.pseudo(v-on:click="removeProp(index)") Remove
-          div.card
-            button(v-on:click="addNewData") Add Data
-            div(v-for="(data, index) in newComponent.data")
-              label.label-header Data
-                input(type="text", v-model="data.name")
-              button.pseudo(v-on:click="removeData(index)") Remove
-          div.card
-            button(v-on:click="addNewWatch") Add Watch
-            div(v-for="(watch, index) in newComponent.watches")
-              label.label-header Watch
-                input(type="text", v-model="watch.name")
-              button.pseudo(v-on:click="removeWatch(index)") Remove
-          div.card
-            button(v-on:click="addNewComputed") Add Computed
-            div(v-for="(computed, index) in newComponent.computed")
-              label.label-header Computed
-                input(type="text", v-model="computed.name")
-              button.pseudo(v-on:click="removeComputed(index)") Remove
-          div.card
-            button(v-on:click="addNewMethod") Add Method
-            div(v-for="(method, index) in newComponent.methods")
-              label.label-header Method
-                input(type="text", v-model="method.name")
-              button.pseudo(v-on:click="removeMethod(index)") Remove
-          //- LifeCycle Hook
-          div.card LifeCycle Hooks
+          hr
+          div.flex.two
+            div
+              label.label-header Template
+              select(v-model="newComponent.selectedTemplate")
+                option(v-for="option in options.template" v-bind:value="option") {{option}}
+            div
+              label.label-header Script
+              select(v-model="newComponent.selectedScript")
+                option(v-for="option in options.scripts" v-bind:value="option") {{option}}
+          div.flex.two
+            div
+              label.label-header Style
+              select(v-model="newComponent.selectedStyle")
+                option(v-for="option in options.styles" v-bind:value="option") {{option}}
+            div
+              label.label-header Scoped Style
+                br
+                input(type="checkbox" v-model="newComponent.selectedScoped")
+                span.checkable on
+        hr
+        div.component-form
+          div.flex.two
+            div
+              div.flex.two
+                h2 Props
+                label.label-header.header
+                  input(type="checkbox" value="selectedPropsValidation", v-model="newComponent.selectedPropsValidation")
+                  span.checkable Validation
+              button(v-on:click="addNewProp") Add
+              div(v-for="(prop, index) in newComponent.props" v-if="newComponent.selectedPropsValidation")
+                div.flex.two
+                  div
+                    label.label-header Name
+                      input(type="text", v-model="prop.name")
+                  div
+                    label.label-header Type
+                      select(v-model="prop.type")
+                        option(v-for="option in options.propsType" v-bind:value="option") {{option}}
+                      div.remove-button-wrapper
+                        button.error.button(v-on:click="removeProp(index)") Remove
+              div(v-for="(prop, index) in newComponent.props" v-else)
+                label.label-header Name
+                  input(type="text", v-model="prop.name")
+                div.remove-button-wrapper
+                  button.error.button(v-on:click="removeProp(index)") Remove
+            div
+              h2 Data
+              button(v-on:click="addNewData") Add
+              div(v-for="(data, index) in newComponent.data")
+                label.label-header Data
+                  input(type="text", v-model="data.name")
+                div.remove-button-wrapper
+                  button.error.button(v-on:click="removeData(index)") Remove
+          hr
+          div.flex.two
+            div
+              h2 Watch
+              button(v-on:click="addNewWatch") Add
+              div(v-for="(watch, index) in newComponent.watches")
+                label.label-header Watch
+                  input(type="text", v-model="watch.name")
+                div.remove-button-wrapper
+                  button.error.button(v-on:click="removeWatch(index)") Remove
+            div
+              h2 Computed
+              button(v-on:click="addNewComputed") Add
+              div(v-for="(computed, index) in newComponent.computed")
+                label.label-header Computed
+                  input(type="text", v-model="computed.name")
+                div.remove-button-wrapper
+                  button.error.button(v-on:click="removeComputed(index)") Remove
+          hr
+          div.flex.two
+            div
+              h2 Method
+              button(v-on:click="addNewMethod") Add
+              div(v-for="(method, index) in newComponent.methods")
+                label.label-header Method
+                  input(type="text", v-model="method.name")
+                div.remove-button-wrapper
+                  button.error.button(v-on:click="removeMethod(index)") Remove
+            div
+              h2 Lifecycle Hooks
+              div
                 label.label-header
                   input(type="checkbox" value="beforeCreate", v-model="newComponent.lifecycleHooks")
                   span.checkable beforeCreate
                 label.label-header
                   input(type="checkbox" value="created", v-model="newComponent.lifecycleHooks")
                   span.checkable created
+              div
                 label.label-header
                   input(type="checkbox" value="beforeMount", v-model="newComponent.lifecycleHooks")
                   span.checkable beforeMount
                 label.label-header
                   input(type="checkbox" value="mounted", v-model="newComponent.lifecycleHooks")
                   span.checkable mounted
+              div
                 label.label-header
                   input(type="checkbox" value="beforeUpdate", v-model="newComponent.lifecycleHooks")
                   span.checkable beforeUpdate
                 label.label-header
                   input(type="checkbox" value="updated", v-model="newComponent.lifecycleHooks")
                   span.checkable updated
+              div
                 label.label-header
                   input(type="checkbox" value="beforeDestroy", v-model="newComponent.lifecycleHooks")
                   span.checkable beforeDestroy
                 label.label-header
                   input(type="checkbox" value="destroyed", v-model="newComponent.lifecycleHooks")
                   span.checkable destroyed
-        //- Right (Result)
-      codemirror.col-md-6(v-bind:code="compiledComponent", v-bind:options="editorOption")
 </template>
 
 <script>
-// function htmlEscape (str) {
-//   return str.replace(/&/g, '&amp;') // first!
-//             .replace(/>/g, '&gt;')
-//             .replace(/</g, '&lt;')
-//             .replace(/"/g, '&quot;')
-//             .replace(/'/g, '&#39;')
-//             .replace(/`/g, '&#96;')
-// }
-//
-// function html (literalSections, ...substs) {
-//   // Use raw literal sections: we don’t want
-//   // backslashes (\n etc.) to be interpreted
-//   let raw = literalSections.raw
-//
-//   let result = ''
-//
-//   substs.forEach((subst, i) => {
-//     // Retrieve the literal section preceding
-//     // the current substitution
-//     let lit = raw[i]
-//
-//     // In the example, map() returns an array:
-//     // If substitution is an array (and not a string),
-//     // we turn it into a string
-//     if (Array.isArray(subst)) {
-//       subst = subst.join('')
-//     }
-//
-//     // If the substitution is preceded by a dollar sign,
-//     // we escape special characters in it
-//     if (lit.endsWith('$')) {
-//       subst = htmlEscape(subst)
-//       lit = lit.slice(0, -1)
-//     }
-//     result += lit
-//     result += subst
-//   })
-//   // Take care of last literal section
-//   // (Never fails, because an empty template string
-//   // produces one literal section, an empty string)
-//   result += raw[raw.length - 1] // (A)
-//
-//   return result
-// }
 import Beautify from 'js-beautify'
 
 // let codemirror
@@ -146,7 +138,8 @@ export default {
         template: ['html', 'pug'],
         styles: ['css', 'scss', 'sass', 'stylus'],
         scripts: ['javascript', 'coffeescript'],
-        scopedStyle: [true, false]
+        scopedStyle: [true, false],
+        propsType: ['null', 'String', 'Number', 'Boolean', 'Function', 'Object', 'Array']
       },
       newComponent: {
         name: '',
@@ -159,6 +152,7 @@ export default {
         selectedTemplate: 'html',
         selectedStyle: 'css',
         selectedScript: 'javascript',
+        selectedPropsValidation: false,
         selectedScoped: false
       },
       editorOption: {
@@ -179,7 +173,7 @@ export default {
   },
   methods: {
     addNewProp () {
-      this.newComponent.props.push({name: ''})
+      this.newComponent.props.push({name: '', type: 'String'})
     },
     removeProp (index) {
       this.newComponent.props.splice(index, 1)
@@ -209,25 +203,6 @@ export default {
       this.newComponent.methods.splice(index, 1)
     }
   },
-  watch: {
-    // compiledComponent: {
-    //   handler (newVal, oldVal) {
-    //     var dom = document.querySelector('.code-mirror')
-    //     if (!codemirror) {
-    //       codemirror = CodeMirror.fromTextArea(dom, {
-    //         lineNumbers: true,
-    //         mode: 'vue',
-    //         height: '600px',
-    //         styleActiveLine: true,
-    //         matchBrackets: true,
-    //         theme: 'base16-dark'
-    //       })
-    //     }
-    //     codemirror.setValue(newVal)
-    //   },
-    //   deep: true
-    // }
-  },
   computed: {
     compiledComponent: function () {
       const component = JSON.parse(JSON.stringify(this.newComponent))
@@ -237,11 +212,23 @@ export default {
       const styleScoped = component.selectedScoped ? ' scoped' : ''
       const scriptLanguage = component.selectedScript === 'javascript' ? '' : ` lang='${component.selectedScript}'`
       let scriptBody = 'export default {'
+      // TODO: Transform "kebab-case"
       scriptBody += 'name: ' + "'" + component.name + "',"
       // Props
+      // TODO: Add Validation
       if (component.props && component.props.length > 0) {
-        let scriptProps = component.props.map(prop => `'${prop.name}'`).join(',')
-        scriptBody += `props: [${scriptProps}],`
+        let scriptProps = component.props.map((prop) => {
+          if (component.selectedPropsValidation) {
+            return `${prop.name}: ${prop.type}`
+          } else {
+            return `'${prop.name}'`
+          }
+        }).join(',')
+        if (component.selectedPropsValidation) {
+          scriptBody += `props: {\n${scriptProps}\n},`
+        } else {
+          scriptBody += `props: [${scriptProps}],`
+        }
       }
       // Check Data
       if (component.data && component.data.length > 0) {
@@ -281,12 +268,12 @@ export default {
         'indent_handlebars': true,
         'object': {}
       })
-      let dummyText = '<!-- This Component is generated by Vue Component Generator - ChangJoo Park. --> \n'
+      // let dummyText = '<!-- This Component is generated by Vue Component Generator - ChangJoo Park. --> \n'
       let templateBody = isHTML ? '\n\t<div>\n\n\t</div>\n' : '\n\tdiv\n'
       let templateResult = '<template' + templateLanguage + '>' + templateBody + '</template>\n\n'
       let scriptResult = '<script' + scriptLanguage + '>\n' + scriptBody + '\n</scri' + 'pt>\n\n'
       let styleResult = '<style' + styleLanguage + styleScoped + '>\n\n' + '</style>'
-      const compiledComponent = dummyText + templateResult + scriptResult + styleResult
+      const compiledComponent = templateResult + scriptResult + styleResult
       return compiledComponent
     }
   }
@@ -298,17 +285,31 @@ export default {
   padding: 30px;
   padding-top: 10px;
 }
-.CodeMirror {
+.container {
   font-family: 'Source Code Pro', 'Monaco','menlo', monospace !important;
-  width: 50% !important;
-  height: 100vh !important;
+  width: 49% !important;
+  height: 100% !important;
+  margin-top: 55px;
 }
-.card {
-  padding: 5px;
-  margin-bottom: 10px;
+.CodeMirror {
+  height: 100vh !important;
+  margin-top: 53px;
+  position: fixed;
+  left: 0;
+  top: 0;
 }
 .label-header {
   font-size: 20px;
   margin-bottom: 10px;
+}
+.remove-button-wrapper {
+  text-align: right;
+}
+.remove-button-wrapper .button {
+  font-size: .75em;
+}
+.label-header.header {
+  padding: .9em;
+  margin: 0;
 }
 </style>
